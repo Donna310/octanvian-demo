b02bcf52f9d029d527eaf552c9a0c91e956a5b00
Donna310 <mythach03102003@gmail.com>
2024-05-19 16:07:01 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'PÔN ';
+const giftUrl = 'http://facebook.com';
+const eventName = 'Gửi Anh ';
+const titleCard = 'Tặng anh iu';
+const contentCard = 'Anh yêu ơi, sinh nhật vui vẻ! Cảm ơn anh vì mọi điều tốt đẹp anh đã mang đến cho cuộc đời em. Mong rằng ngày sinh nhật của anh sẽ đầy niềm vui và hạnh phúc, như anh vẫn mang lại cho em .Em biết rằng tình yêu của chúng ta sẽ cùng trải qua mọi thăng trầm, và em thật sự cảm kích vì sự hiện diện quan trọng của anh trong cuộc đời em.Đặc biệt hơn hết là em mong rằng tất cả mơ ước của anh sẽ thành hiện thực. Em iu anh nhiều lắm ạ.';
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
